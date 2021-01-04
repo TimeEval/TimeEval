de2ccb288e02eee97a5ed4acd39e5181af7f90fe
wenig <info@pwenig.de>
2021-01-04 13:41:31 +0100

    feat: - add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="TimeEval",
+    version="0.1",
+    description="Evaluation Tool for Time Series Anomaly Detection",
+    author="Phillip Wenig",
+    author_email="phillip.wenig@hpi.de",
+    packages=find_packages()
+)
