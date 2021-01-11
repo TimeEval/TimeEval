50a6d68414fcd6c5a11e535e6e14580e24ccad25
wenig <info@pwenig.de>
2021-01-11 13:17:55 +0100

    feat: bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="TimeEval",
-    version="0.1.6",
+    version="0.1.7",
     description="Evaluation Tool for Time Series Anomaly Detection",
     author="Phillip Wenig",
     author_email="phillip.wenig@hpi.de",
