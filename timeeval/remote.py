a7fe83724babdf3869ed75a35439710d6848c799
CodeLionX <sebastian.schmidl@t-online.de>
2021-02-12 21:58:50 +0100

    wip

--- timeeval/remote.py
+++ timeeval/remote.py
@@ -33,7 +33,7 @@ class RemoteConfiguration:
             # https://distributed.dask.org/en/latest/worker.html?highlight=worker_options#distributed.worker.Worker
             "worker_options": {
                 "nprocs": 1,
-                "nthreads": 2
+                "nthreads": 1
             },
             # defaults are fine: https://distributed.dask.org/en/latest/scheduling-state.html?highlight=dask.distributed.Scheduler#distributed.scheduler.Scheduler
             # "scheduler_options": {},
