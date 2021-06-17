3d45f355b4fb4b8dd33d0d1c1cade31c8e3c0928
CodeLionX <sebastian.schmidl@t-online.de>
2021-06-17 10:05:49 +0200

    refactor: move timeout configuration to resource constraints and distinguish between train and execute timeouts; add an option to ignore timeouts from training tasks

--- tests/test_resource_constraints.py
+++ tests/test_resource_constraints.py
@@ -14,7 +14,7 @@ class TestResourceConstraints(unittest.TestCase):
 
     def test_default(self):
         limits = ResourceConstraints()
-        mem, cpu = limits.get_resource_limits()
+        mem, cpu = limits.get_compute_resource_limits()
 
         self.assertEqual(mem, self.usable_memory)
         self.assertEqual(cpu, self.usable_cpus)
@@ -22,7 +22,7 @@ class TestResourceConstraints(unittest.TestCase):
     def test_default_from_tasks_per_host(self):
         tasks = 2
         limits = ResourceConstraints(tasks_per_host=tasks)
-        mem, cpu = limits.get_resource_limits()
+        mem, cpu = limits.get_compute_resource_limits()
 
         self.assertEqual(mem, self.usable_memory / tasks)
         self.assertEqual(cpu, self.usable_cpus / tasks)
@@ -32,13 +32,13 @@ class TestResourceConstraints(unittest.TestCase):
         cpu_limit = 1.256
         mem, cpu = ResourceConstraints(
             task_memory_limit=mem_limit
-        ).get_resource_limits()
+        ).get_compute_resource_limits()
         self.assertEqual(mem, mem_limit)
         self.assertEqual(cpu, self.usable_cpus)
 
         mem, cpu = ResourceConstraints(
             task_cpu_limit=cpu_limit
-        ).get_resource_limits()
+        ).get_compute_resource_limits()
         self.assertEqual(mem, self.usable_memory)
         self.assertEqual(cpu, cpu_limit)
 
@@ -47,7 +47,7 @@ class TestResourceConstraints(unittest.TestCase):
         mem_overwrite = 1325
         cpu_overwrite = 1.256
         limits = ResourceConstraints(tasks_per_host=tasks, task_memory_limit=12)
-        mem, cpu = limits.get_resource_limits(
+        mem, cpu = limits.get_compute_resource_limits(
             memory_overwrite=mem_overwrite,
             cpu_overwrite=cpu_overwrite,
         )
