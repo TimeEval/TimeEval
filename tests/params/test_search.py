8aa6fa72ef50a178bed638393ec498c72152aba7
CodeLionX <sebastian.schmidl@t-online.de>
2021-09-30 15:26:19 +0200

    chore: add parameter grid classes and tests forthem (forgot to add them)

---
+++ tests/params/test_search.py
@@ -0,0 +1,68 @@
+import unittest
+
+from timeeval.params import FullParameterGrid, IndependentParameterGrid
+
+
+class TestParameterSearch(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.params = {"a": [1, 2], "b": [True, False], "c": ["auto", "tanh"]}
+
+    def test_full_parameter_grid(self):
+        grid = FullParameterGrid(self.params)
+        self.assertListEqual(list(grid), [
+            {"a": 1, "b": True, "c": "auto"},
+            {"a": 1, "b": True, "c": "tanh"},
+            {"a": 1, "b": False, "c": "auto"},
+            {"a": 1, "b": False, "c": "tanh"},
+            {"a": 2, "b": True, "c": "auto"},
+            {"a": 2, "b": True, "c": "tanh"},
+            {"a": 2, "b": False, "c": "auto"},
+            {"a": 2, "b": False, "c": "tanh"}
+        ])
+        self.assertEqual(grid[1], {"a": 1, "b": True, "c": "tanh"})
+
+    def test_full_parameter_grid_wrong_input(self):
+        with self.assertRaises(TypeError) as ex:
+            FullParameterGrid([{"a": [1, 2]}])  # type: ignore
+            self.assertRegex(str(ex.exception), "[P|p]lease use a.*IndependentParameterGrid.")
+
+        with self.assertRaises(TypeError) as ex:
+            FullParameterGrid("a")  # type: ignore
+            self.assertRegex(str(ex.exception), "[P|p]arameter grid is not a dict")
+
+    def test_independent_parameter_grid(self):
+        grid = IndependentParameterGrid(self.params)
+        self.assertListEqual(list(grid), [
+            {"a": 1},
+            {"a": 2},
+            {"b": True},
+            {"b": False},
+            {"c": "auto"},
+            {"c": "tanh"}
+        ])
+        self.assertEqual(grid[2], {"b": True})
+
+    def test_independent_parameter_grid_with_defaults(self):
+        grid = IndependentParameterGrid(self.params, {"a": 0, "c": "auto"})
+        self.assertListEqual(list(grid), [
+            {"a": 1, "c": "auto"},
+            {"a": 2, "c": "auto"},
+            {"a": 0, "b": True, "c": "auto"},
+            {"a": 0, "b": False, "c": "auto"},
+            {"a": 0, "c": "auto"},
+            {"a": 0, "c": "tanh"}
+        ])
+
+    def test_independent_parameter_grid_wrong_input(self):
+        with self.assertRaises(TypeError) as ex1:
+            IndependentParameterGrid([{"a": [1, 2]}])  # type: ignore
+            self.assertRegex(str(ex1.exception), "[P|p]arameter grid is not a dict")
+
+        with self.assertRaises(TypeError) as ex2:
+            IndependentParameterGrid({"a": [1, 2]}, default_params=[1])  # type: ignore
+            self.assertRegex(str(ex2.exception), "[D|d]efault parameters grid is not a dict")
+
+    def test_independent_parameter_grid_non_value_list(self):
+        grid = IndependentParameterGrid({"a": [1, 2], "b": True})
+        self.assertListEqual(list(grid), [{"a": 1}, {"a": 2}, {"b": True}])
