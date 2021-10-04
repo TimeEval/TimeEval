43161e644827ac3bca38e6a1d8bc7cf1f85d3249
wenig <info@pwenig.de>
2021-10-04 11:51:38 +0200

    test: add tests for default heuristic

--- tests/heuristics/test_default_exponential_factor_heuristic.py
+++ tests/heuristics/test_default_exponential_factor_heuristic.py
@@ -45,3 +45,15 @@ class TestDefaultExponentialFactorHeuristic(unittest.TestCase):
         with self.assertRaises(ValueError) as e:
             heuristic(fixtures.algorithm, fixtures.dataset, fixtures.dummy_dataset_path, param_name="beta")
         self.assertIn("Could not find the default value", str(e.exception))
+
+    def test_returns_expected_data_type_int(self):
+        self.algo.params["beta"]["defaultValue"] = 1
+        heuristic = DefaultExponentialFactorHeuristic(exponent=2)
+        value = heuristic(self.algo, fixtures.dataset, fixtures.dummy_dataset_path, param_name="beta")
+        self.assertEqual(type(value), int)
+
+    def test_returns_expected_data_type_float(self):
+        self.algo.params["beta"]["defaultValue"] = 1.0
+        heuristic = DefaultExponentialFactorHeuristic(exponent=2)
+        value = heuristic(self.algo, fixtures.dataset, fixtures.dummy_dataset_path, param_name="beta")
+        self.assertEqual(type(value), float)
