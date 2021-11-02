e1848aba06b719eb0e5406c39f6877bc12c474fb
CodeLionX <sebastian.schmidl@t-online.de>
2021-11-02 14:34:44 +0100

    process: prepare nondeterminism debug run

---
+++ timeeval_experiments/2021-11-02-debug-nondeterminism.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import logging
+import random
+import shutil
+import sys
+from typing import List, Tuple
+
+import numpy as np
+from durations import Duration
+
+from timeeval import TimeEval, Datasets
+from timeeval.constants import HPI_CLUSTER
+from timeeval.remote import RemoteConfiguration
+from timeeval.resource_constraints import ResourceConstraints, GB
+from timeeval.utils.metrics import Metric
+from timeeval_experiments.algorithm_configurator import AlgorithmConfigurator
+from timeeval_experiments.algorithms import *
+
+
+# Setup logging
+logging.basicConfig(
+    filename="timeeval.log",
+    filemode="a",
+    level=logging.INFO,
+    force=True,
+    format="%(asctime)s %(levelname)6.6s - %(name)20.20s: %(message)s",
+)
+
+random.seed(42)
+np.random.rand(42)
+
+
+def main():
+    dm = Datasets(HPI_CLUSTER.akita_benchmark_path, create_if_missing=False)
+    configurator = AlgorithmConfigurator(config_path="param-config.json")
+
+    # Select datasets and algorithms
+    datasets: List[Tuple[str, str]] = []
+    datasets += dm.select(collection_name="Keogh")
+    datasets += dm.select(collection_name="KDD-TSAD", dataset_name="239_UCR_Anomaly_taichidbS0715Master")
+    datasets += dm.select(collection_name="KDD-TSAD", dataset_name="240_UCR_Anomaly_taichidbS0715Master")
+    datasets += dm.select(collection_name="Exathlon", dataset_name="4_1_100000_61-29")
+    print(f"Selecting {len(datasets)} datasets")
+
+    algorithms = [
+        lof(),
+        ssa(),
+        stamp(),
+    ]
+    print(f"Selecting {len(algorithms)} algorithms")
+
+    print("Configuring algorithms...")
+    configurator.configure(algorithms, perform_search=False)
+
+    print("\nDatasets:")
+    print("=====================================================================================")
+    for collection in np.unique([c for (c, d) in datasets]):
+        print(collection)
+        cds = sorted([d for (c, d) in datasets if c == collection])
+        for cd in cds:
+            print(f"  {cd}")
+    print("=====================================================================================\n\n")
+
+    print("\nParameter configurations:")
+    print("=====================================================================================")
+    for algo in algorithms:
+        print(algo.name)
+        for param in algo.param_grid:
+            print(f"  {param}")
+    print("=====================================================================================\n\n")
+    sys.stdout.flush()
+
+    cluster_config = RemoteConfiguration(
+        scheduler_host=HPI_CLUSTER.odin01,
+        worker_hosts=HPI_CLUSTER.nodes
+    )
+    limits = ResourceConstraints(
+        tasks_per_host=10,
+        task_cpu_limit=1.,
+        task_memory_limit=3*GB,
+        train_fails_on_timeout=False,
+        train_timeout=Duration("2 hours"),
+        execute_timeout=Duration("2 hours"),
+    )
+    timeeval = TimeEval(dm, datasets, algorithms,
+                        repetitions=1,
+                        distributed=True,
+                        remote_config=cluster_config,
+                        resource_constraints=limits,
+                        skip_invalid_combinations=True,
+                        force_dimensionality_match=False,
+                        force_training_type_match=False,
+                        metrics=[Metric.ROC_AUC, Metric.PR_AUC, Metric.RANGE_PR_AUC, Metric.AVERAGE_PRECISION],
+                        )
+
+    # copy parameter configuration file to results folder
+    timeeval.results_path.mkdir(parents=True)
+    shutil.copy2(configurator.config_path, timeeval.results)
+
+    timeeval.run()
+    print(timeeval.get_results(aggregated=True, short=True))
+
+
+if __name__ == "__main__":
+    main()
