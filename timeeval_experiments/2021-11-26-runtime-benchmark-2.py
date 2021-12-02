9d4d245fc12ec0457e807a09c81e6c336d5da089
CodeLionX <sebastian.schmidl@t-online.de>
2021-12-02 15:10:28 +0100

    chore: remove re-execution file

--- timeeval_experiments/2021-11-26-runtime-benchmark-2.py
+++ timeeval_experiments/2021-11-26-runtime-benchmark-2.py
@@ -194,7 +194,7 @@ def main():
                         force_dimensionality_match=False,
                         force_training_type_match=False,
                         metrics=[Metric.ROC_AUC, Metric.PR_AUC, Metric.RANGE_PR_AUC, Metric.AVERAGE_PRECISION],
-                        experiment_combinations_file=Path("/home/projects/akita/results/2021-11-26_runtime-benchmark-2/re-execution-experiments.csv")
+                        # experiment_combinations_file=Path("/home/projects/akita/results/2021-11-26_runtime-benchmark-2/re-execution-experiments.csv")
                         )
 
     # copy parameter configuration file to results folder
