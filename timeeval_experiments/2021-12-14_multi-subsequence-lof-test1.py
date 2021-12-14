cc0d7c4d47615d4f82b44461752fc16f02c3ad46
CodeLionX <sebastian.schmidl@t-online.de>
2021-12-14 10:41:50 +0100

    wip

--- timeeval_experiments/2021-12-14_multi-subsequence-lof-test1.py
+++ timeeval_experiments/2021-12-14_multi-subsequence-lof-test1.py
@@ -38,13 +38,45 @@ def main():
     #     root_data_path / "test-cases"
     # )
     dm = DatasetManager(
-        HPI_CLUSTER.akita_correlation_anomalies_path
+        HPI_CLUSTER.akita_benchmark_path
     )
 
     # Select datasets and algorithms
     datasets: List[Tuple[str, str]] = []
-    # use only unsupervised datasets for the multivariate Subsequence LOF algorithm of the GutenTAG collection
-    datasets += dm.select(collection="GutenTAG", training_type=TrainingType.UNSUPERVISED)
+    datasets += dm.select(collection="CalIt2", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="Daphnet", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # no datasets match criteria for Dodgers
+    # datasets += dm.select(collection="Dodgers", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # select 4 datasets of large-timeseries collection Exathlon
+    datasets += random.sample(dm.select(collection="Exathlon", training_type=TrainingType.SUPERVISED), 2)
+    datasets += random.sample(dm.select(collection="Exathlon", training_type=TrainingType.SEMI_SUPERVISED), 2)
+    datasets += dm.select(collection="GHL", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="Genesis", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # GutenTAG uses a separate run
+    # select 4 datasets of large-timeseries collection IOPS
+    datasets += random.sample(dm.select(collection="IOPS", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES), 4)
+    # include everything from KDD-TSAD!
+    datasets += dm.select(collection="KDD-TSAD")
+    datasets += dm.select(collection="Keogh", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # exclude Kitsune completely, bc it's too large!
+    # datasets += random.sample(dm.select(collection="Kitsune", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES), 4)
+    # exclude LTDB completely, bc it's too large!
+    # datasets += random.sample(dm.select(collection="LTDB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES), 4)
+    datasets += dm.select(collection="MGAB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="MITDB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="Metro", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # include everything from NAB!
+    datasets += dm.select(collection="NAB")
+    datasets += dm.select(collection="NASA-MSL", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="NASA-SMAP", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="OPPORTUNITY", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # no datasets match criteria for Occupancy
+    datasets += dm.select(collection="Occupancy", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="SMD", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # no datasets match criteria for SSA, bc contamination > 0.14 for all datasets
+    # datasets += dm.select(collection="SSA")
+    datasets += dm.select(collection="SVDB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="WebscopeS5", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
     print(f"Selecting {len(datasets)} datasets")
 
     algorithms = [
