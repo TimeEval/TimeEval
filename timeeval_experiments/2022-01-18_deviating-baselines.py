4e23ba0253cb6645f779c679e2851f92bacf3795
CodeLionX <sebastian.schmidl@t-online.de>
2022-01-18 15:12:48 +0100

    process: add script to test deviating baselines on all datasets

---
+++ timeeval_experiments/2022-01-18_deviating-baselines.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+import logging
+import random
+import shutil
+import sys
+from typing import List, Tuple
+
+import numpy as np
+from durations import Duration
+
+from timeeval import TimeEval, DatasetManager, MultiDatasetManager, TrainingType
+from timeeval.constants import HPI_CLUSTER
+from timeeval.remote import RemoteConfiguration
+from timeeval.resource_constraints import ResourceConstraints, GB
+from timeeval.utils.metrics import Metric
+from timeeval_experiments.algorithm_configurator import AlgorithmConfigurator
+from timeeval_experiments.algorithms import *
+from timeeval_experiments.baselines import Baselines
+
+
+# Setup logging
+logging.basicConfig(
+    filename="timeeval.log",
+    filemode="a",
+    level=logging.INFO,
+    force=True,
+    format="%(asctime)s %(levelname)6.6s - %(name)20.20s: %(message)s",
+)
+
+random.seed(42)
+np.random.rand(42)
+MAX_CONTAMINATION = 0.1
+MIN_ANOMALIES = 1
+
+
+def main():
+    dm = MultiDatasetManager([
+        HPI_CLUSTER.akita_benchmark_path,
+        HPI_CLUSTER.akita_test_case_path,
+        HPI_CLUSTER.akita_correlation_anomalies_path
+    ])
+    configurator = AlgorithmConfigurator(config_path="param-config.json")
+
+    # Select datasets and algorithms
+    datasets: List[Tuple[str, str]] = []
+    datasets += dm.select(collection="SWaT")
+    datasets += dm.select(collection="WADI")
+    datasets += dm.select(collection="CalIt2", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="Daphnet", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # no datasets match criteria for Dodgers
+    # datasets += dm.select(collection="Dodgers", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # select 4 datasets of large-timeseries collection Exathlon
+    datasets += random.sample(dm.select(collection="Exathlon", training_type=TrainingType.SUPERVISED.value), 2)
+    datasets += random.sample(dm.select(collection="Exathlon", training_type=TrainingType.SEMI_SUPERVISED.value), 2)
+    datasets += dm.select(collection="GHL", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="Genesis", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # GutenTAG uses a separate run
+    # select 4 datasets of large-timeseries collection IOPS
+    datasets += random.sample(dm.select(collection="IOPS", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES), 4)
+    # include everything from KDD-TSAD!
+    datasets += dm.select(collection="KDD-TSAD")
+    datasets += dm.select(collection="Keogh", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # exclude Kitsune completely, bc it's too large!
+    # datasets += random.sample(dm.select(collection="Kitsune", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES), 4)
+    # exclude LTDB completely, bc it's too large!
+    # datasets += random.sample(dm.select(collection="LTDB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES), 4)
+    datasets += dm.select(collection="MGAB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="MITDB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="Metro", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # include everything from NAB!
+    datasets += dm.select(collection="NAB")
+    datasets += dm.select(collection="NASA-MSL", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="NASA-SMAP", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="OPPORTUNITY", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # no datasets match criteria for Occupancy
+    datasets += dm.select(collection="Occupancy", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="SMD", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # no datasets match criteria for SSA, bc contamination > 0.14 for all datasets
+    # datasets += dm.select(collection="SSA")
+    datasets += dm.select(collection="SVDB", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    datasets += dm.select(collection="WebscopeS5", max_contamination=MAX_CONTAMINATION, min_anomalies=MIN_ANOMALIES)
+    # add the new datasets as well
+    datasets += dm.select(collection="WADI")
+    datasets += dm.select(collection="SWaT")
+    # add the GutenTAG datasets (includes the correlation anomalies)
+    datasets += dm.select(collection="GutenTAG")
+    print(f"Selecting {len(datasets)} datasets")
+
+    algorithms = [
+        Baselines.deviating_from_mean(),
+        Baselines.deviating_from_median()
+    ]
+    print(f"Selecting {len(algorithms)} algorithms")
+
+    print("Configuring algorithms...")
+    configurator.configure(algorithms, perform_search=False)
+
+    print("\nDatasets:")
+    print("=====================================================================================")
+    for collection in np.unique([c for (c, d) in datasets]):
+        print(collection)
+        cds = sorted([d for (c, d) in datasets if c == collection])
+        for cd in cds:
+            print(f"  {cd}")
+    print("=====================================================================================\n\n")
+
+    print("\nParameter configurations:")
+    print("=====================================================================================")
+    for algo in algorithms:
+        print(algo.name)
+        for param in algo.param_config:
+            print(f"  {param}")
+    print("=====================================================================================\n\n")
+    sys.stdout.flush()
+
+    cluster_config = RemoteConfiguration(
+        scheduler_host=HPI_CLUSTER.odin01,
+        worker_hosts=HPI_CLUSTER.nodes
+    )
+    limits = ResourceConstraints(
+        tasks_per_host=10,
+        task_cpu_limit=1.,
+        task_memory_limit=3*GB,
+        train_fails_on_timeout=False,
+        train_timeout=Duration("2 hours"),
+        execute_timeout=Duration("2 hours"),
+    )
+    timeeval = TimeEval(dm, datasets, algorithms,
+                        repetitions=1,
+                        distributed=True,
+                        remote_config=cluster_config,
+                        resource_constraints=limits,
+                        skip_invalid_combinations=True,
+                        metrics=[Metric.ROC_AUC, Metric.PR_AUC, Metric.RANGE_PR_AUC, Metric.AVERAGE_PRECISION],
+                        )
+    # copy parameter configuration file to results folder
+    timeeval.results_path.mkdir(parents=True, exist_ok=True)
+    shutil.copy2(configurator.config_path, timeeval.results_path)
+
+    timeeval.run()
+    print(timeeval.get_results(aggregated=True, short=True))
+
+
+if __name__ == "__main__":
+    main()
