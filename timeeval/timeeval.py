570faa03b0206742a7b57fec80f9bd3bbefa6a4d
CodeLionX <sebastian.schmidl@t-online.de>
2022-05-27 14:27:53 +0200

    fix: errors caused by name-change

--- timeeval/timeeval.py
+++ timeeval/timeeval.py
@@ -242,10 +242,10 @@ class TimeEval:
 
         df = self.results
 
-        if Status.ERROR.name in df.status.unique() or Status.TIMEOUT.name in df.status.unique():
+        if Status.ERROR in df.status.unique() or Status.TIMEOUT in df.status.unique():
             self.log.warning("The results contain errors which are filtered out for the final aggregation. "
                              "To see all results, call .get_results(aggregated=False)")
-            df = df[df.status == Status.OK.name]
+            df = df[df.status == Status.OK]
 
         if short:
             time_names = ["train_main_time", "execute_main_time"]
