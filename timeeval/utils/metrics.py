81784a00caf05401468625193e67af36ee902592
CodeLionX <sebastian.schmidl@t-online.de>
2022-06-30 17:54:44 +0200

    refactor: allow metrics to expose configuration options by implementing them as classes

--- timeeval/utils/metrics.py
+++ timeeval/utils/metrics.py
@@ -1,3 +1,4 @@
+import abc
 import warnings
 from enum import Enum
 from typing import Iterable, Callable, List, Tuple
@@ -8,28 +9,15 @@ from sklearn.metrics import precision_recall_curve, roc_curve, auc, average_prec
 from sklearn.utils import column_or_1d, assert_all_finite, check_consistent_length
 
 
-class Metric(Enum):
-    ROC_AUC = 0
-    PR_AUC = 1
-    RANGE_PR_AUC = 2
-    RANGE_PRECISION = 3
-    RANGE_RECALL = 4
-    RANGE_F1 = 5
-    AVERAGE_PRECISION = 6
-
-    @staticmethod
-    def default() -> 'Metric':
-        return Metric.ROC_AUC
-
-    @staticmethod
-    def default_list() -> List['Metric']:
-        return [Metric.ROC_AUC]
+class ABCMetric(abc.ABC):
+    def __call__(self, y_true: np.ndarray, y_score: np.ndarray, **kwargs) -> float:
+        y_true, y_score = self._validate_scores(y_true, y_score, **kwargs)
+        return self.score(y_true, y_score)
 
     def _validate_scores(self, y_true: np.ndarray, y_score: np.ndarray,
                          inf_is_1: bool = True,
                          neginf_is_0: bool = True,
-                         nan_is_0: bool = True,
-                         **kwargs) -> Tuple[np.ndarray, np.ndarray]:
+                         nan_is_0: bool = True) -> Tuple[np.ndarray, np.ndarray]:
         y_true = np.array(y_true).copy()
         y_score = np.array(y_score).copy()
         # check labels
@@ -45,14 +33,14 @@ class Metric(Enum):
         y_score: np.ndarray = column_or_1d(y_score)  # type: ignore
 
         check_consistent_length([y_true, y_score])
-        if self not in [Metric.ROC_AUC, Metric.PR_AUC, Metric.RANGE_PR_AUC, Metric.AVERAGE_PRECISION]:
+        if not self.supports_continuous_scorings():
             if y_score.dtype != np.int_:
-                raise ValueError("When using Metrics other than AUC-metric (like Precision, Recall or F1-Score), "
-                                 "the scores must be integers and have the values {0, 1}. Please consider applying "
-                                 "a threshold to the scores!")
+                raise ValueError("When using Metrics other than AUC-metric that need discrete (0 or 1) scores (like "
+                                 "Precision, Recall or F1-Score), the scores must be integers and should only contain"
+                                 "the values {0, 1}. Please consider applying a threshold to the scores!")
         else:
             if y_score.dtype != np.float_:
-                raise ValueError("When using AUC-based metrics, the scores must be floats!")
+                raise ValueError("When using continuous scoring metrics, the scores must be floats!")
 
         # substitute NaNs and Infs
         nan_mask = np.isnan(y_score)
@@ -76,87 +64,302 @@ class Metric(Enum):
         assert_all_finite(y_score)
         return y_true, y_score
 
-    def __call__(self, y_true: np.ndarray, y_score: np.ndarray, **kwargs) -> float:
-        y_true, y_score = self._validate_scores(y_true, y_score, **kwargs)
+    @abc.abstractmethod
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        ...
 
-        if self == Metric.ROC_AUC:
-            return auc_roc(y_true, y_score, **kwargs)
-        elif self == Metric.RANGE_PRECISION:
-            return ts_precision(y_true, y_score, **kwargs)
-        elif self == Metric.RANGE_RECALL:
-            return ts_recall(y_true, y_score, **kwargs)
-        elif self == Metric.RANGE_F1:
-            return ts_fscore(y_true, y_score, **kwargs)
-        elif self == Metric.RANGE_PR_AUC:
-            return auc_range_precision_recall_curve(y_true, y_score, **kwargs)
-        elif self == Metric.PR_AUC:
-            return auc_precision_recall_curve(y_true, y_score, **kwargs)
-        else:  # if self == Metric.AVERAGE_PRECISION:
-            kwargs.pop("plot", None)
-            kwargs.pop("plot_store", None)
-            return average_precision_score(y_true, y_score, pos_label=1, **kwargs)
+    @abc.abstractmethod
+    def supports_continuous_scorings(self) -> bool:
+        ...
 
 
-def _metric(y_true: np.ndarray, y_score: Iterable[float], _curve_function: Callable, plot: bool = False, **kwargs) -> float:
+def _auc_metric(y_true: np.ndarray, y_score: Iterable[float], _curve_function: Callable,
+                plot: bool = False,
+                plot_store: bool = False) -> float:
     x, y, thresholds = _curve_function(y_true, y_score)
     if "precision_recall" in _curve_function.__name__:
         # swap x and y
         x, y = y, x
     area = auc(x, y)
     if plot:
-        store = False
-        if "plot_store" in kwargs:
-            store = kwargs["plot_store"]
-
         import matplotlib.pyplot as plt
 
         name = _curve_function.__name__
         plt.plot(x, y, label=name, drawstyle="steps-post")
         # plt.plot([0, 1], [0, 1], linestyle="--", label="Random")
         plt.title(f"{name} | area = {area:.4f}")
-        if store:
+        if plot_store:
             plt.savefig(f"fig-{name}.pdf")
         plt.show()
     return area
 
 
-def range_precision_recall_curve(y_true: np.ndarray, y_score: np.ndarray, max_samples=50) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    thresholds = np.unique(y_score)
-    thresholds.sort()
-    # The first precision and recall values are precision=class balance and recall=1.0, which corresponds to a
-    # classifier that always predicts the positive class, independently of the threshold. This means that we can
-    # skip the first threshold!
-    p0 = y_true.sum() / len(y_true)
-    r0 = 1.0
-    thresholds = thresholds[1:]
+class RocAUC(ABCMetric):
+    def __init__(self, plot: bool = False, plot_store: bool = False):
+        self._plot = plot
+        self._plot_store = plot_store
 
-    # sample thresholds
-    n_thresholds = thresholds.shape[0]
-    if n_thresholds > max_samples:
-        every_nth = n_thresholds // (max_samples - 1)
-        sampled_thresholds = thresholds[::every_nth]
-        if thresholds[-1] == sampled_thresholds[-1]:
-            thresholds = sampled_thresholds
-        else:
-            thresholds = np.r_[sampled_thresholds, thresholds[-1]]
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        return _auc_metric(y_true, y_score, roc_curve, plot=self._plot, plot_store=self._plot_store)
 
-    recalls = np.zeros_like(thresholds)
-    precisions = np.zeros_like(thresholds)
-    for i, threshold in enumerate(thresholds):
-        y_pred = (y_score >= threshold).astype(np.int64)
-        recalls[i] = ts_recall(y_true, y_pred)
-        precisions[i] = ts_precision(y_true, y_pred)
-    sorted_idx = recalls.argsort()[::-1]
-    return np.r_[p0, precisions[sorted_idx], 1], np.r_[r0, recalls[sorted_idx], 0], thresholds
+    def supports_continuous_scorings(self) -> bool:
+        return True
 
 
-def auc_roc(y_true: np.ndarray, y_score: Iterable[float], **kwargs) -> float:
-    return _metric(y_true, y_score, roc_curve, **kwargs)
+class PrAUC(ABCMetric):
+    """Computes the area under the precision recall curve.
+    """
+
+    def __init__(self, plot: bool = False, plot_store: bool = False):
+        self._plot = plot
+        self._plot_store = plot_store
+
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        return _auc_metric(y_true, y_score, precision_recall_curve, plot=self._plot, plot_store=self._plot_store)
+
+    def supports_continuous_scorings(self) -> bool:
+        return True
 
 
-def auc_precision_recall_curve(y_true: np.ndarray, y_score: np.ndarray, **kwargs) -> float:
-    return _metric(y_true, y_score, precision_recall_curve, **kwargs)
+class RangePrAUC(ABCMetric):
+    """Computes the area under the precision recall curve when using the range-based precision and range-based
+    recall metric introduced by Tatbul et al. at NeurIPS 2018 [1]_.
+
+    Parameters
+    ----------
+    max_samples: int
+        TimeEval uses a community implementation of the range-based precision and recall metrics, which is quite slow.
+        To prevent long runtimes caused by scorings with high precision (many thresholds), just a specific amount of
+        possible thresholds is sampled. This parameter controls the maximum number of thresholds; too low numbers
+        degrade the metrics' quality.
+    r_alpha : float
+        Weight of the existence reward for the range-based recall.
+    p_alpha : float
+        Weight of the existence reward for the range-based precision. For most - when not all - cases, `p_alpha`
+        should be set to 0.
+    cardinality : {'reciprocal', 'one', 'udf_gamma'}
+        Cardinality type.
+    bias : {'flat', 'front', 'middle', 'back'}
+        Positional bias type.
+    plot : bool
+    plot_store : bool
+
+    .. [1] Tatbul, Nesime, Tae Jun Lee, Stan Zdonik, Mejbah Alam, and Justin Gottschlich. "Precision and Recall for
+       Time Series." In Proceedings of the International Conference on Neural Information Processing Systems (NeurIPS),
+       1920–30. 2018. http://papers.nips.cc/paper/7462-precision-and-recall-for-time-series.pdf.
+    """
+
+    def __init__(self,
+                 max_samples: int = 50,
+                 r_alpha: float = 0.5,
+                 p_alpha: float = 0,
+                 cardinality: str = "reciprocal",
+                 bias: str = "flat",
+                 plot: bool = False,
+                 plot_store: bool = False):
+        self._plot = plot
+        self._plot_store = plot_store
+        self._max_samples = max_samples
+        self._r_alpha = r_alpha
+        self._p_alpha = p_alpha
+        self._cardinality = cardinality
+        self._bias = bias
+
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        return _auc_metric(y_true, y_score, self._range_precision_recall_curve, plot=self._plot, plot_store=self._plot_store)
+
+    def _range_precision_recall_curve(self, y_true: np.ndarray, y_score: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        thresholds = np.unique(y_score)
+        thresholds.sort()
+        # The first precision and recall values are precision=class balance and recall=1.0, which corresponds to a
+        # classifier that always predicts the positive class, independently of the threshold. This means that we can
+        # skip the first threshold!
+        p0 = y_true.sum() / len(y_true)
+        r0 = 1.0
+        thresholds = thresholds[1:]
+
+        # sample thresholds
+        n_thresholds = thresholds.shape[0]
+        if n_thresholds > self._max_samples:
+            every_nth = n_thresholds // (self._max_samples - 1)
+            sampled_thresholds = thresholds[::every_nth]
+            if thresholds[-1] == sampled_thresholds[-1]:
+                thresholds = sampled_thresholds
+            else:
+                thresholds = np.r_[sampled_thresholds, thresholds[-1]]
+
+        recalls = np.zeros_like(thresholds)
+        precisions = np.zeros_like(thresholds)
+        for i, threshold in enumerate(thresholds):
+            y_pred = (y_score >= threshold).astype(np.int64)
+            recalls[i] = ts_recall(y_true, y_pred,
+                                   alpha=self._r_alpha,
+                                   cardinality=self._cardinality,
+                                   bias=self._bias)
+            precisions[i] = ts_precision(y_true, y_pred,
+                                         alpha=self._p_alpha,
+                                         cardinality=self._cardinality,
+                                         bias=self._bias)
+        sorted_idx = recalls.argsort()[::-1]
+        return np.r_[p0, precisions[sorted_idx], 1], np.r_[r0, recalls[sorted_idx], 0], thresholds
+
+    def supports_continuous_scorings(self) -> bool:
+        return True
 
 
-def auc_range_precision_recall_curve(y_true: np.ndarray, y_score: np.ndarray, **kwargs) -> float:
-    return _metric(y_true, y_score, range_precision_recall_curve, **kwargs)
+class AveragePrecision(ABCMetric):
+    """Computes the average precision metric aver all possible thresholds.
+
+    Parameters
+    ----------
+    kwargs : dict
+        Keyword arguments that get passed down to :func:`sklearn.metrics._ranking.average_precision_score`
+
+    See Also
+    --------
+    sklearn.metrics._ranking.average_precision_score : Implementation of the average precision metric.
+    """
+
+    def __init__(self, **kwargs):
+        self._kwargs = kwargs
+
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        return average_precision_score(y_true, y_score, pos_label=1, **self._kwargs)
+
+    def supports_continuous_scorings(self) -> bool:
+        return True
+
+
+class RangePrecision(ABCMetric):
+    """Computes the range-based precision metric introduced by Tatbul et al. at NeurIPS 2018 [1]_.
+
+    Parameters
+    ----------
+    alpha : float
+        Weight of the existence reward. For most - when not all - cases, `p_alpha` should be set to 0.
+    cardinality : {'reciprocal', 'one', 'udf_gamma'}
+        Cardinality type.
+    bias : {'flat', 'front', 'middle', 'back'}
+        Positional bias type.
+
+    .. [1] Tatbul, Nesime, Tae Jun Lee, Stan Zdonik, Mejbah Alam, and Justin Gottschlich. "Precision and Recall for
+       Time Series." In Proceedings of the International Conference on Neural Information Processing Systems (NeurIPS),
+       1920–30. 2018. http://papers.nips.cc/paper/7462-precision-and-recall-for-time-series.pdf.
+    """
+
+    def __init__(self, alpha: float = 0, cardinality: str = "reciprocal", bias: str = "flat"):
+        self._alpha = alpha
+        self._cardinality = cardinality
+        self._bias = bias
+
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        return ts_precision(y_true, y_score, alpha=self._alpha, cardinality=self._cardinality, bias=self._bias)
+
+    def supports_continuous_scorings(self) -> bool:
+        return False
+
+
+class RangeRecall(ABCMetric):
+    """Computes the range-based recall metric introduced by Tatbul et al. at NeurIPS 2018 [1]_.
+
+    Parameters
+    ----------
+    alpha : float
+        Weight of the existence reward. If 0: no existence reward, if 1: only existence reward.
+    cardinality : {'reciprocal', 'one', 'udf_gamma'}
+        Cardinality type.
+    bias : {'flat', 'front', 'middle', 'back'}
+        Positional bias type.
+
+    .. [1] Tatbul, Nesime, Tae Jun Lee, Stan Zdonik, Mejbah Alam, and Justin Gottschlich. "Precision and Recall for
+       Time Series." In Proceedings of the International Conference on Neural Information Processing Systems (NeurIPS),
+       1920–30. 2018. http://papers.nips.cc/paper/7462-precision-and-recall-for-time-series.pdf.
+    """
+
+    def __init__(self, alpha: float = 0, cardinality: str = "reciprocal", bias: str = "flat"):
+        self._alpha = alpha
+        self._cardinality = cardinality
+        self._bias = bias
+
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        return ts_recall(y_true, y_score, alpha=self._alpha, cardinality=self._cardinality, bias=self._bias)
+
+    def supports_continuous_scorings(self) -> bool:
+        return False
+
+
+class RangeFScore(ABCMetric):
+    """Computes the range-based F-score using the recall and precision metrics by Tatbul et al. at NeurIPS 2018 [1]_.
+
+    The F-beta score is the weighted harmonic mean of precision and recall, reaching its optimal value at 1 and its
+    worst value at 0. This implementation uses the range-based precision and range-based recall as basis.
+
+    Parameters
+    ----------
+    beta : float
+        F-score beta determines the weight of recall in the combined score.
+        beta < 1 lends more weight to precision, while beta > 1 favors recall.
+    p_alpha : float
+        Weight of the existence reward for the range-based precision. For most - when not all - cases, `p_alpha`
+        should be set to 0.
+    r_alpha : float
+        Weight of the existence reward. If 0: no existence reward, if 1: only existence reward.
+    cardinality : {'reciprocal', 'one', 'udf_gamma'}
+        Cardinality type.
+    p_bias : {'flat', 'front', 'middle', 'back'}
+        Positional bias type.
+    r_bias : {'flat', 'front', 'middle', 'back'}
+        Positional bias type.
+
+    .. [1] Tatbul, Nesime, Tae Jun Lee, Stan Zdonik, Mejbah Alam, and Justin Gottschlich. "Precision and Recall for
+       Time Series." In Proceedings of the International Conference on Neural Information Processing Systems (NeurIPS),
+       1920–30. 2018. http://papers.nips.cc/paper/7462-precision-and-recall-for-time-series.pdf.
+    """
+
+    def __init__(self,
+                 beta: float = 1,
+                 p_alpha: float = 0,
+                 r_alpha: float = 0.5,
+                 cardinality: str = "reciprocal",
+                 p_bias: str = "flat",
+                 r_bias: str = "flat"):
+        self._beta = beta
+        self._p_alpha = p_alpha
+        self._r_alpha = r_alpha
+        self._cardinality = cardinality
+        self._p_bias = p_bias
+        self._r_bias = r_bias
+
+    def score(self, y_true: np.ndarray, y_score: np.ndarray) -> float:
+        return ts_fscore(y_true, y_score,
+                         beta=self._beta,
+                         p_alpha=self._p_alpha, r_alpha=self._r_alpha,
+                         cardinality=self._cardinality,
+                         p_bias=self._p_bias, r_bias=self._p_bias)
+
+    def supports_continuous_scorings(self) -> bool:
+        return False
+
+
+class Metric(Enum):
+    ROC_AUC = RocAUC()
+    PR_AUC = PrAUC()
+    RANGE_PR_AUC = RangePrAUC(r_alpha=0, cardinality="one", bias="flat")
+    AVERAGE_PRECISION = AveragePrecision()
+    RANGE_PRECISION = RangePrecision()
+    RANGE_RECALL = RangeRecall()
+    RANGE_F1 = RangeFScore(beta=1)
+
+    def __init__(self, m_repr: ABCMetric):
+        self._internal: ABCMetric = m_repr
+
+    def __call__(self, y_true: np.ndarray, y_score: np.ndarray, **kwargs) -> float:
+        return self._internal(y_true, y_score, **kwargs)
+
+    @staticmethod
+    def default() -> 'Metric':
+        return Metric.ROC_AUC
+
+    @staticmethod
+    def default_list() -> List['Metric']:
+        return [Metric.ROC_AUC]
