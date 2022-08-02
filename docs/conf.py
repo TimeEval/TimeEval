068085886498b63385f33afeacdee2d80206c11c
CodeLionX <sebastian.schmidl@t-online.de>
2022-08-02 16:34:56 +0200

    feat: link to more external docs

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,8 @@ myst_heading_anchors = 3
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'dask': ('https://docs.dask.org/en/stable/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'pandas': ('https://pandas.pydata.org/docs/', None)
+    'numpy': ('https://numpy.org/doc/1.21/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/version/1.3/', None),
+    'sklearn': ('https://scikit-learn.org/0.24/', None),
+    'statsmodels': ('https://www.statsmodels.org/v0.12.0/', None),
 }
