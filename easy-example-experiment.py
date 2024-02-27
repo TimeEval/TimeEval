883fda591cec46fa8d301a615a46ea1bcaf333ce
Shubham Mamgain <46908976+mshubham00@users.noreply.github.com>
2024-02-27 16:02:38 +0100

    Update easy-example-experiment.py
    calling COF algorithm as a function

--- easy-example-experiment.py
+++ easy-example-experiment.py
@@ -1,46 +1,37 @@
 #!/usr/bin/env python3
 from pathlib import Path
 from typing import Any, Dict
-
 import numpy as np
 
 from timeeval import TimeEval, DatasetManager, DefaultMetrics, Algorithm, TrainingType, InputDimensionality
-from timeeval.adapters import DockerAdapter, FunctionAdapter
-from timeeval.algorithms import cof
+from timeeval.adapters import DockerAdapter
+from timeeval.adapters import FunctionAdapter # for defining customized algorithm
+from timeeval.algorithms import cof 
 from timeeval.data_types import AlgorithmParameter
 from timeeval.params import FixedParameters
-
 
 def your_algorithm_function(data: AlgorithmParameter, args: Dict[str, Any]) -> np.ndarray:
     if isinstance(data, np.ndarray):
         return np.zeros_like(data)
-    else:  # isinstance(data, pathlib.Path)
+    else:
         return np.genfromtxt(data, delimiter=",", skip_header=1)[:, 1]
-
 
 def main():
     dm = DatasetManager(Path("tests/example_data"), create_if_missing=False)
-    datasets = dm.select()
-
+    datasets = dm.select()    
+    
     algorithms = [
-        Algorithm(
-            name="COF",
-            main=DockerAdapter(image_name="ghcr.io/timeeval/cof", skip_pull=True),
-            param_config=FixedParameters({
-                "n_neighbors": 20,
-                "random_state": 42
-            }),
-            data_as_file=True,
-            training_type=TrainingType.UNSUPERVISED,
-            input_dimensionality=InputDimensionality.MULTIVARIATE
+    # list of algorithms which will be implimented on the selected dataset(s)
+        cof(
+            params=FixedParameters({
+                "n_neighbors": 20, 
+                "random_state": 42})
         ),
-        # or:
-        # cof(params=FixedParameters({"n_neighbors": 20, "random_state": 42})),
+        # calling customized algorithm
         Algorithm(
             name="MyPythonFunctionAlgorithm",
             main=FunctionAdapter(your_algorithm_function),
-            data_as_file=False
-        )
+            data_as_file=False)
     ]
 
     timeeval = TimeEval(dm, datasets, algorithms, metrics=[DefaultMetrics.ROC_AUC, DefaultMetrics.RANGE_PR_AUC])
