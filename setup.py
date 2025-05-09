c80d15b9b9796930d9dac7aaa0447f9d23ecbbda
CodeLionX <sebastian.schmidl@t-online.de>
2025-05-09 15:56:26 +0200

    feat: update build setup, remove old python support, and add new python version support

--- setup.py
+++ setup.py
@@ -15,18 +15,6 @@ with open(Path("timeeval") / "_version.py") as f:
 VERSION: str = __version__  # noqa
 
 
-def load_dependencies():
-    EXCLUDES = ["python", "pip"]
-    with open(HERE / "requirements.txt", "r", encoding="UTF-8") as f:
-        env = f.readlines()
-
-    def excluded(name):
-        return any([excl in name for excl in EXCLUDES])
-
-    deps = [dep for dep in env if not excluded(dep)]
-    return deps
-
-
 class PyTestCommand(Command):
     description = "run PyTest for TimeEval"
     user_options = []
@@ -70,7 +58,7 @@ class MyPyCheckCommand(Command):
         from mypy.main import main as mypy
 
         args = ["--pretty", "timeeval", "timeeval_experiments"]
-        mypy(None, stdout=sys.stdout, stderr=sys.stderr, args=args)
+        mypy(args=args, stdout=sys.stdout, stderr=sys.stderr)
 
 
 class CleanCommand(Command):
@@ -104,29 +92,12 @@ class CleanCommand(Command):
 
 if __name__ == "__main__":
     setup(
-        name="TimeEval",
         version=VERSION,
-        description="Evaluation Tool for Time Series Anomaly Detection Methods",
         long_description=README,
         long_description_content_type="text/markdown",
-        author="Phillip Wenig and Sebastian Schmidl",
-        author_email="phillip.wenig@hpi.de",
-        url="https://github.com/TimeEval/TimeEval",
-        license="MIT",
-        classifiers=[
-            "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11"
-        ],
         packages=find_packages(exclude=("tests", "tests.*")),
+        url="https://github.com/TimeEval/TimeEval",
         package_data={"timeeval": ["py.typed"], "timeeval_experiments": ["py.typed"]},
-        install_requires=load_dependencies(),
-        python_requires=">=3.7, <=3.11",
-        test_suite="tests",
         cmdclass={
             "test": PyTestCommand,
             "typecheck": MyPyCheckCommand,
--- timeeval/_version.py
+++ timeeval/_version.py
@@ -1,2 +1,2 @@
-__version__: str = "1.4.2"
+__version__: str = "1.5.0pre1"
 """Version of this TimeEval installation"""
